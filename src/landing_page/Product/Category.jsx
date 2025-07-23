73d11cac09ff2cba9970b389acc250fa05dedacc
Brijesh Kumar <iconbrijesh@gmail.com>
2025-07-23 15:36:42 +0530

    built Category comp

--- src/landing_page/Product/Category.jsx
+++ src/landing_page/Product/Category.jsx
@@ -1,6 +1,38 @@
-function Category(){
-    return(
-        <div>This is category</div>
-    )
+import React from 'react';
+import './Category.css'; // ← Import CSS
+
+const categories = [
+  {
+    name: 'Personal Care & Hygiene',
+    image: 'https://cdn-icons-png.flaticon.com/512/892/892458.png',
+  },
+  {
+    name: 'Home Cleaning Supplies',
+    image: 'https://cdn-icons-png.flaticon.com/512/1046/1046756.png',
+  },
+  {
+    name: 'Clothing',
+    image: 'https://cdn-icons-png.flaticon.com/512/892/892458.png',
+  },
+  {
+    name: 'Gardening & Composting',
+    image: 'https://cdn-icons-png.flaticon.com/512/2902/2902142.png',
+  },
+  {
+    name: 'Kitchen Essentials',
+    image: 'https://cdn-icons-png.flaticon.com/512/1046/1046857.png',
+  },
+];
+
+export default function Category() {
+  return (
+    <div className="category-container">
+      {categories.map((cat, index) => (
+        <div className="category-card" key={index}>
+          <img src={cat.image} alt={cat.name} />
+          <p>{cat.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
-export default Category;
